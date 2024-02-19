4f4030a1a5eb8580a9fceb1698f173d6d6ec5f72
warabimochi <satomi920003@yahoo.co.jp>
2024-02-19 20:24:21 +0900

    4

---
+++ app/nested-layout/second/layout.tsx
@@ -0,0 +1,8 @@
+export default function SecondLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <p>Layout 2</p>
+            { children }
+        </>
+    )
+}
---
+++ app/nested-layout/second/third/fourth/page.tsx
@@ -0,0 +1,7 @@
+export default function ForthPage() {
+    return (
+        <div className="m-10 text-center">
+            <p>Page 4</p>
+        </div>
+    )
+}
